cbb32046474120e8cd0922bda7a46526af6eba35
sarcaxticlarka <osmsad786@gmail.com>
2023-10-20 22:38:28 +0530

    sidcup

---
+++ script.js
@@ -0,0 +1,116 @@
+var crsr = document.querySelector("#cursor");
+var crsrb = document.querySelector("#cursor-blur");
+
+document.addEventListener("mousemove", function(dets) {
+    crsr.style.left = dets.x+30 + "px";  
+    crsr.style.top = dets.y + "px";   
+    crsrb.style.left = dets.x -200 + "px";  
+    crsrb.style.top = dets.y  -200 + "px";   
+})
+
+var h4all = document.querySelectorAll("#nav h4")
+h4all.forEach(function(elem){
+    elem.addEventListener("mouseenter", function(){
+        crsr.style.scale = 3.5
+        crsr.style.border = "1px solid white"
+        crsr.style.backgroundColor = "transparent"
+    })
+    elem.addEventListener("mouseleave", function(){
+        crsr.style.scale = 1
+        crsr.style.border = "0px solid #befc12"
+        crsr.style.backgroundColor = "#befc12"
+    })
+})
+
+
+gsap.to("#nav",{
+    backgroundColor: "black",
+    height:"100px",
+    duration: 0.5,
+    scrollTrigger:{
+        trigger:"#nav",
+        scroller:"body",
+        start:"top -10%",
+        end:"top -11%",
+        scrub: 1
+
+    }     
+})
+
+gsap.to("#main",{
+    backgroundColor: "black",
+    scrollTrigger:{
+        trigger:"#main",
+        scroller:"body",
+        // markers: true,
+        start:"top -25%",
+        end:"top -75%",
+        scrub: 2
+
+    }   
+})
+gsap.from("#about-us img, #about-us-in",{
+     y: 50,
+     opacity:0,
+     duration:1,
+     stagger:0.1,
+     scrollTrigger:{
+        trigger:"#about-us",
+        scroller:"body",
+        start:"top 70%",
+        end:"top 26%",
+        scrub:1
+     }
+
+     
+})
+
+gsap.from(".card",{
+    scale: 0.8,
+    opacity:0,
+    duration:1,
+    stagger:0.4,
+    scrollTrigger:{
+       trigger:".card",
+       scroller:"body",
+       start:"top 70%",
+       end:"top 65%",
+       scrub:1
+    }   
+})
+
+gsap.from("#clone1", {
+    y: -70,
+    x: -70,
+    scrollTrigger: {
+        trigger: "#clone1",
+        scroller: "body",
+        start: "top 55%",  
+        end: "top 45%",    
+        scrub: 4
+    }
+});
+
+gsap.from("#clone2", {
+    y: -70,
+    x: -70,
+    scrollTrigger: {
+        trigger: "#clone2",
+        scroller: "body",
+        start: "top 75%",  
+        end: "top 65%",    
+        scrub: 4
+    }
+});
+
+gsap.from("#page4 h1",{
+    y:50,
+    scrollTrigger: {
+        trigger: "#clone2",
+        scroller: "body",
+        start: "top 45%",  
+        end: "top 40%",    
+        scrub: 4
+    }
+})
+
